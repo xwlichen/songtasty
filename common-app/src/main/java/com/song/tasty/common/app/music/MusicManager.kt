a2b2b61749315c7f3f8674dd3f3b916ddbbc5a27
lichen <1960003945@qq.com>
2020-01-09 18:01:49 +0800

    初步提交

--- common-app/src/main/java/com/song/tasty/common/app/music/MusicManager.kt
+++ common-app/src/main/java/com/song/tasty/common/app/music/MusicManager.kt
@@ -1,5 +1,10 @@
 package com.song.tasty.common.app.music
 
+import com.hjq.toast.ToastUtils
+import com.smart.media.player.SmartMusicPlayer
+import com.smart.media.player.bean.InfoBean
+import com.smart.media.player.enums.InfoCode
+import com.smart.media.player.interf.IPlayer
 import com.song.tasty.common.app.music.bean.MusicBean
 import com.song.tasty.common.app.music.emums.PlayModel
 
@@ -9,9 +14,11 @@ import com.song.tasty.common.app.music.emums.PlayModel
  * @email : 1960003945@qq.com
  * @description :
  */
-class MusicManager private constructor() {
+class MusicManager {
 
-    var musicList: MutableList<MusicBean> = ArrayList()
+
+    lateinit var musicPlayer: SmartMusicPlayer
+    var musicList: ArrayList<MusicBean>
     var playModel: PlayModel = PlayModel.CIRCLE
     var currentIndex: Int = 0;
 
@@ -25,14 +32,48 @@ class MusicManager private constructor() {
     var currentStatus: Int = 0;
 
 
+    private constructor() {
+
+        musicList = ArrayList()
+        initPlayer()
+
+    }
+
     //静态方法、变量
     companion object {
         val instance: MusicManager by lazy(LazyThreadSafetyMode.SYNCHRONIZED) { MusicManager() }
 
     }
 
-    fun setDataList(list: MutableList<MusicBean>) {
 
+    fun initPlayer() {
+        musicPlayer = SmartMusicPlayer()
+
+
+        musicPlayer.setOnPreparedListener(object : IPlayer.OnPreparedListener {
+            override fun onPrepared() {
+                musicPlayer.start()
+            }
+
+        })
+
+        musicPlayer.setOnInfoListener(object : IPlayer.OnInfoListener {
+            override fun onInfo(data: InfoBean?) {
+                var code = data?.code ?: -1;
+                when (code) {
+                    InfoCode.CURRENT_PROGRESS -> {
+                        var time = data?.extraValue.toString()
+                        ToastUtils.show(time)
+                    }
+                }
+            }
+        })
+    }
+
+
+    fun setDataList(list: ArrayList<MusicBean>) {
+        musicList.clear()
+        musicList.addAll(list)
     }
 
     fun playMusic(bean: MusicBean) {
@@ -40,18 +81,27 @@ class MusicManager private constructor() {
         musicList.add(bean)
         currentIndex = 0;
 
-
     }
 
     fun play() {
+        musicList.isNotEmpty().let {
+            if (currentIndex < musicList.size) {
+                musicPlayer.setDataSource(musicList[currentIndex].url)
+                musicPlayer.prepare()
+            }
+
+        }
+
 
     }
 
     fun pause() {
+        musicPlayer.pause()
 
     }
 
     fun resume() {
+        musicPlayer.resume()
 
     }
 
@@ -60,18 +110,35 @@ class MusicManager private constructor() {
      * 单位：s
      */
     fun seek(second: Int) {
+        musicPlayer.seek(second)
 
     }
 
     fun pre() {
+        currentIndex -= 1
+        musicList.isNotEmpty().let {
+            if (currentIndex < musicList.size) {
+                musicPlayer.next(musicList[currentIndex].url)
+            }
+
+        }
 
     }
 
     fun next() {
+        currentIndex + 1
+        musicList.isNotEmpty().let {
+            if (currentIndex < musicList.size) {
+                musicPlayer.next(musicList[currentIndex].url)
+            }
+
+        }
 
     }
 
     fun stop() {
+
+        musicPlayer.stop()
 
     }
 
