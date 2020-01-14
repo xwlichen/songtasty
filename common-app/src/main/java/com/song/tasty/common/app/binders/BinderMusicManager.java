7a86f17d56b6e90a2fbc11abd561bfa09e5e25e5
lichen <1960003945@qq.com>
2020-01-14 11:45:03 +0800

    公共模块不添加kotlin代码

---
+++ common-app/src/main/java/com/song/tasty/common/app/binders/BinderMusicManager.java
@@ -0,0 +1,11 @@
+package com.song.tasty.common.app.binders;
+
+
+/**
+ * @date : 2020-01-13 18:16
+ * @author: lichen
+ * @email : 1960003945@qq.com
+ * @description :
+ */
+public class BinderMusicManager {
+}
