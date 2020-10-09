702e203b726c04c0963d4802cd98ceeadf539869
101011734 <lichen5@huaqin.com>
2020-10-09 19:45:17 +0800

    初步提交

--- common-ui/src/main/java/com/song/tasty/common/ui/widget/behavior/HeaderBehavior.java
+++ common-ui/src/main/java/com/song/tasty/common/ui/widget/behavior/HeaderBehavior.java
@@ -2,10 +2,15 @@ package com.song.tasty.common.ui.widget.behavior;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
+import android.view.VelocityTracker;
 import android.view.View;
+import android.view.ViewConfiguration;
+import android.view.ViewGroup;
 import android.widget.OverScroller;
 import android.widget.ScrollView;
+import android.widget.Scroller;
 
 import androidx.annotation.NonNull;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
@@ -18,6 +23,8 @@ import com.song.tasty.common.ui.R;
 import com.song.tasty.common.ui.widget.NestedLinearLayout;
 
 import java.lang.ref.WeakReference;
+
+import static androidx.constraintlayout.widget.Constraints.TAG;
 
 /**
  * @date : 2019-09-05 17:24
@@ -53,6 +60,26 @@ public class HeaderBehavior extends ViewOffsetBehavior<View> {
     //tab上移结束后是否悬浮在固定位置
     private boolean tabSuspension = false;
 
+    /**
+     * 滑动的最大速度
+     */
+    private int maximumVelocity;
+    /**
+     * 滑动的最小速度
+     */
+    private int minimumVelocity;
+
+    /**
+     * 滑动线程
+     */
+//    private HeaderFlingRunnable fling;
+
+
+    /**
+     * 滑动速度追踪
+     */
+    private VelocityTracker velocityTracker;
+
     public HeaderBehavior() {
         init();
     }
@@ -65,6 +92,9 @@ public class HeaderBehavior extends ViewOffsetBehavior<View> {
 
     private void init() {
         mOverScroller = new OverScroller(mContext);
+        maximumVelocity = ViewConfiguration.get(mContext).getScaledMaximumFlingVelocity();
+        minimumVelocity = ViewConfiguration.get(mContext).getScaledMinimumFlingVelocity();
+//        fling = new FlingRunnable(mContext);
     }
 
     //可以重写这个方法对子View 进行重新布局
@@ -130,8 +160,8 @@ public class HeaderBehavior extends ViewOffsetBehavior<View> {
      * @param coordinatorLayout
      * @param child
      * @param target
-     * @param velocityX x 方向的速度
-     * @param velocityY y 方向的速度
+     * @param velocityX         x 方向的速度
+     * @param velocityY         y 方向的速度
      * @return
      */
     @Override
@@ -144,22 +174,61 @@ public class HeaderBehavior extends ViewOffsetBehavior<View> {
 //            LogUtils.e("xw", "flag false");
 //
 //        }
-        return false;
+        LogUtils.e("xw", "onNestedPreFling velocityY:" + velocityY);
+//        return super.onNestedPreFling(coordinatorLayout, child, target, velocityX, velocityY);
+        return true;
     }
 
     @Override
+    public boolean onNestedFling(@NonNull CoordinatorLayout coordinatorLayout, @NonNull View child, @NonNull View target, float velocityX, float velocityY, boolean consumed) {
+        LogUtils.e("xw", "onNestedFling velocityY："+velocityY);
+        return super.onNestedFling(coordinatorLayout, child, target, velocityX, velocityY, consumed);
+    }
+
+
+    @Override
     public boolean onInterceptTouchEvent(CoordinatorLayout parent, final View child, MotionEvent ev) {
+        LogUtils.e("xw", "onInterceptTouchEvent");
+        if (velocityTracker == null) {
+            velocityTracker = VelocityTracker.obtain();
+        }
+        velocityTracker.addMovement(ev);
         switch (ev.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 downReach = false;
                 upReach = false;
                 mOverScroller.abortAnimation();
                 break;
-            case MotionEvent.ACTION_UP:
-//                handleActionUp(child);
-                break;
+//            case MotionEvent.ACTION_UP:
+////                mParent = new WeakReference<CoordinatorLayout>(parent);
+////                handleActionUp(child);
+//                LogUtils.e("xw", "onInterceptTouchEvent ACTION_UP");
+//                // 计算当前速度， 1000表示每秒像素数等
+//                velocityTracker.computeCurrentVelocity(1000, maximumVelocity);
+//
+//                // 获取横向速度
+//                int velocityY = (int) velocityTracker.getYVelocity();
+//
+//                // 速度要大于最小的速度值，才开始滑动
+//                if (Math.abs(velocityY) > minimumVelocity) {
+//
+//                    int initY = child.getScrollY();
+//
+//                    int maxY = (int) (child.getHeight());
+//
+////                    if (maxX > 0) {
+//                        fling(initY,velocityY,0,maxY);
+////                    }
+//                }
+//
+//                if (velocityTracker != null) {
+//                    velocityTracker.recycle();
+//                    velocityTracker = null;
+//                }
+//                break;
         }
         return super.onInterceptTouchEvent(parent, child, ev);
+//        return true;
     }
 
 
@@ -192,8 +261,8 @@ public class HeaderBehavior extends ViewOffsetBehavior<View> {
     @Override
     public void onNestedPreScroll(@NonNull CoordinatorLayout coordinatorLayout, @NonNull View child, @NonNull View target, int dx, int dy, @NonNull int[] consumed, int type) {
         super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed, type);
-
-        LogUtils.e("xw","type:"+type);
+        LogUtils.e("xw","onNestedPreScroll");
+//        LogUtils.e("xw","type:"+type);
 
         //制造滑动视察，使header的移动比手指滑动慢
         float scrollY = dy / 4.0f;
@@ -279,6 +348,28 @@ public class HeaderBehavior extends ViewOffsetBehavior<View> {
         super.onNestedScrollAccepted(coordinatorLayout, child, directTargetChild, target, nestedScrollAxes);
     }
 
+    @Override
+    public boolean onTouchEvent(@NonNull CoordinatorLayout parent, @NonNull View child, @NonNull MotionEvent ev) {
+        LogUtils.e("xw","onTouchEvent");
+
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_MOVE:
+                break;
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_CANCEL:
+                mParent = new WeakReference<CoordinatorLayout>(parent);
+                LogUtils.e("xw", "ACTION_UP");
+//                handleActionUp(child);
+                break;
+            default:
+                break;
+        }
+//        return super.onTouchEvent(parent, child, ev);
+
+        return true;
+    }
+
+
     /**
      * 是否可以整体滑动
      *
@@ -304,7 +395,12 @@ public class HeaderBehavior extends ViewOffsetBehavior<View> {
         return mContext.getResources().getDimensionPixelOffset(R.dimen.dp_030_negative);
     }
 
+    private void onFlingFinished(View layout) {
+        changeState(isClosed(layout) ? STATE_CLOSED : STATE_OPENED);
+    }
+
     private void handleActionUp(View child) {
+        mChild = new WeakReference<View>(child);
         if (mFlingRunnable != null) {
             child.removeCallbacks(mFlingRunnable);
             mFlingRunnable = null;
@@ -315,10 +411,6 @@ public class HeaderBehavior extends ViewOffsetBehavior<View> {
         } else {
             scrollToOpen(DURATION_SHORT);
         }
-    }
-
-    private void onFlingFinished(View layout) {
-        changeState(isClosed(layout) ? STATE_CLOSED : STATE_OPENED);
     }
 
     /**
@@ -390,6 +482,24 @@ public class HeaderBehavior extends ViewOffsetBehavior<View> {
         start();
     }
 
+    private int mInitY;
+    private int mMinY;
+    private int mMaxY;
+    private int mVelocityY;
+    private void fling(int initY,
+                       int velocityY,
+                       int minY,
+                       int maxY ){
+        this.mInitY= initY;
+        this.mVelocityY= velocityY;
+        this.mMinY = minY;
+        this.mMaxY = maxY;
+        float curTranslationY = mChild.get().getTranslationY();
+        mOverScroller.fling(0, (int) curTranslationY, 0,velocityY,0,0,0,maxY);
+        start();
+    }
+
+
     private void start() {
         if (mOverScroller.computeScrollOffset()) {
             mFlingRunnable = new FlingRunnable(mParent.get(), mChild.get());
@@ -434,4 +544,121 @@ public class HeaderBehavior extends ViewOffsetBehavior<View> {
 
         void onHeaderOpened();
     }
+
+
+
+
+    /**
+     * 滚动线程
+     */
+//    private class HeaderFlingRunnable implements Runnable {
+//
+////        private Scroller mScroller;
+//
+//        private int mInitX;
+//        private int mMinX;
+//        private int mMaxX;
+//        private int mVelocityX;
+//        private final CoordinatorLayout mParent;
+//        private final View mLayout;
+//
+//        HeaderFlingRunnable(CoordinatorLayout parent, View layout) {
+////            this.mScroller = new Scroller(context, null, false);
+//            mParent = parent;
+//            mLayout = layout;
+//        }
+//
+//        HeaderFlingRunnable(Context context) {
+////            this.mScroller = new Scroller(context, null, false);
+//        }
+//
+//        void start(int initX,
+//                   int velocityX,
+//                   int minX,
+//                   int maxX) {
+//            this.mInitX = initX;
+//            this.mVelocityX = velocityX;
+//            this.mMinX = minX;
+//            this.mMaxX = maxX;
+//
+//            // 先停止上一次的滚动
+//            if (!mScroller.isFinished()) {
+//                mScroller.abortAnimation();
+//            }
+//
+//            // 开始 fling
+//            mScroller.fling(initX, 0, velocityX,
+//                    0, 0, maxX, 0, 0);
+//            post(this);
+//        }
+//
+//        @Override
+//        public void run() {
+//            if (mLayout==null||mOverScroller==null){
+//                return;
+//            }
+//            if (mLayout != null && mOverScroller != null) {
+//                if (mOverScroller.computeScrollOffset()) {
+//                    mLayout.setTranslationY(mOverScroller.getCurrY());
+//                    ViewCompat.postOnAnimation(mLayout, this);
+//                } else {
+//                    onFlingFinished(mLayout);
+//                }
+//            }
+//
+//
+//            // 如果已经结束，就不再进行
+//            if (!mOverScroller.computeScrollOffset()) {
+//                return;
+//            }
+//
+//            // 计算偏移量
+//            int currX = mOverScroller.getCurrX();
+//            int diffX = mInitX - currX;
+//
+//            Log.i("xw", "run: [currX: " + currX + "]\n"
+//                    + "[diffX: " + diffX + "]\n"
+//                    + "[initX: " + mInitX + "]\n"
+//                    + "[minX: " + mMinX + "]\n"
+//                    + "[maxX: " + mMaxX + "]\n"
+//                    + "[velocityX: " + mVelocityX + "]\n"
+//            );
+//
+//            // 用于记录是否超出边界，如果已经超出边界，则不再进行回调，即使滚动还没有完成
+//            boolean isEnd = false;
+//
+//            if (diffX != 0) {
+//
+//                // 超出右边界，进行修正
+//                if (mLayout.getScrollX() + diffX >= mCanvasWidth - mViewWidth) {
+//                    diffX = (int) (mCanvasWidth - mViewWidth - getScrollX());
+//                    isEnd = true;
+//                }
+//
+//                // 超出左边界，进行修正
+//                if (getScrollX() <= 0) {
+//                    diffX = -getScrollX();
+//                    isEnd = true;
+//                }
+//
+//                if (!mScroller.isFinished()) {
+//                    scrollBy(diffX, 0);
+//                }
+//                mInitX = currX;
+//            }
+//
+//            if (!isEnd) {
+//                post(this);
+//            }
+//        }
+//
+//        /**
+//         * 进行停止
+//         */
+//        void stop() {
+//            if (!mOverScroller.isFinished()) {
+//                mOverScroller.abortAnimation();
+//            }
+//        }
+//    }
 }
