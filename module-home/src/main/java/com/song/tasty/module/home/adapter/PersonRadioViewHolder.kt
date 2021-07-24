bf2c5844fc00f2a273e3feff69d31ff6de3b8060
101011734 <lichen5@huaqin.com>
2021-07-24 15:43:01 +0800

    初步提交

---
+++ module-home/src/main/java/com/song/tasty/module/home/adapter/PersonRadioViewHolder.kt
@@ -0,0 +1,28 @@
+package com.song.tasty.module.home.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.song.tasty.common.app.base.BaseRecyclerAdapter
+import com.song.tasty.module.home.entity.SongBean
+import me.drakeet.multitype.ItemViewBinder
+
+class PersonRadioViewHolder: ItemViewBinder<SongBean, PersonRadioViewHolder.ViewHolder>() {
+
+
+
+
+    class ViewHolder(itemView: View) :RecyclerView.ViewHolder(itemView){
+
+    }
+
+    override fun onCreateViewHolder(inflater: LayoutInflater, parent: ViewGroup): ViewHolder {
+        val view=inflater.inflate(R.layout.home_item_person_radio,parent,false)
+        return ViewHolder(view!!)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, item: SongBean) {
+        TODO("Not yet implemented")
+    }
+}
--- module-home/src/main/java/com/song/tasty/module/home/mvvm/ui/PersonRadioActivity.kt
+++ module-home/src/main/java/com/song/tasty/module/home/mvvm/ui/PersonRadioActivity.kt
@@ -1,16 +1,43 @@
 package com.song.tasty.module.home.mvvm.ui
 
 
+import android.graphics.drawable.GradientDrawable
+import androidx.recyclerview.widget.OrientationHelper
 import com.smart.ui.widget.SMUITopBar
 import com.song.tasty.common.app.base.BaseAppActivity
+import com.song.tasty.common.ui.widget.recycler.PagerLayoutManager
 import com.song.tasty.module.home.R
 import com.song.tasty.module.home.mvvm.viewmodel.PersonRadioViewModel
+import kotlinx.android.synthetic.main.home_activity_person_radio.*
+import me.drakeet.multitype.Items
+import me.drakeet.multitype.MultiTypeAdapter
 
 class PersonRadioActivity : BaseAppActivity<PersonRadioViewModel>() {
+
+    private val mPagerLayoutManager by lazy {
+        PagerLayoutManager(this,OrientationHelper.VERTICAL)
+    }
+
+    private val mAdapter by lazy {
+        MultiTypeAdapter()
+    }
+
+    private val mItems by lazy {
+        Items()
+    }
 
 
     override fun getLayoutResId(): Int {
         //return 0;
         return R.layout.home_activity_person_radio;
+    }
+
+    override fun initView() {
+        super.initView()
+        rvContainer.apply {
+            layoutManager = mPagerLayoutManager
+            adapter = mAdapter
+        }
+
     }
 }
